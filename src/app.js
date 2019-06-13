ed9857c64e11d21639e0ba426ae507b2aa256b1e
andluo <1455234749@qq.com>
2019-06-13 14:01:11 +0800

    完成input组件

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 
 import Button from './components/button'
 import ButtonGroup from './components/button-group'
+import Input from './components/input'
 
 Vue.component('g-button',Button);
 Vue.component('g-button-group',ButtonGroup);
+Vue.component('g-input',Input);
 
 new Vue({
     el:"#app",
---
+++ src/components/input.vue
@@ -0,0 +1,126 @@
+<template>
+
+    <div class="g-input" :class="classes">
+        <span v-if="label">{{ label }}</span>
+        <input :value="value" :placeholder="placeholder" :readonly="disable" />
+        <template v-if="errMsg">
+            <div class="errWrapper">
+                <Icon name="error" class="error"></Icon>
+                <span  class="error">{{ errMsg }}</span>
+            </div>
+        </template>
+    </div>
+
+    
+</template>
+
+<script>
+    import Icon from './icon'
+    export default {
+        name: "GearInput",
+        components:{
+            Icon
+        },
+        props:{
+            value:{
+              type:String
+            },
+            disable:{
+              type:Boolean,
+              default:false
+            },
+            placeholder:{
+                type:String
+            },
+            label:{
+                type:String
+            },
+            errMsg:{
+                type:String
+            },
+            direction:{
+                type:String,
+                default:''
+            }
+        },
+
+        computed:{
+            classes:function () {
+                return [`${this.direction}`,this.disable?'disable':'']
+            }
+        }
+    }
+
+</script>
+
+<style scoped lang="scss">
+    $input-height:32px;
+    $border-radius:4px;
+    $border-color:#999;
+    $border-color-hover: #666;
+    $font-color:#333;
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
+    $errMsg-font-size:12px;
+
+    .g-input {
+        display: inline-flex;
+        align-items: center;
+        color: $font-color;
+        margin-bottom: 10px; //测试
+        > :not(:last-child) {margin-right: .5em; }
+
+        > input {
+            color: $font-color;
+            height: $input-height;
+            border-radius: $border-radius;
+            border: 1px solid $border-color;
+            padding: 0 .4em;
+
+            &:focus {
+                box-shadow: inset 0 1px 3px $box-shadow-color; outline: none;
+            }
+            &:hover {
+                border-color: $border-color-hover;
+            }
+
+            &::-webkit-input-placeholder { /* WebKit browsers */
+                color:    #ccc;
+            }
+            &:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+                color:    #ccc;
+            }
+            &::-moz-placeholder { /* Mozilla Firefox 19+ */
+                color:    #ccc;
+            }
+            &:-ms-input-placeholder { /* Internet Explorer 10+ */
+                color:    #ccc;
+            }
+        }
+
+        > .errWrapper {
+            color:#F1453D;
+            font-size: $errMsg-font-size;
+            display: inline-block;
+        }
+
+        &.vertical {
+            flex-direction: column;
+            align-items: start;
+
+            > :not(:last-child) {margin-right: 0; margin-bottom: .5em;}
+        }
+
+        &.disable {
+            >input {
+                border-color:#66666642;
+                cursor:not-allowed;
+
+                &:focus{
+                    box-shadow:none;
+
+                }
+            }
+        }
+
+    }
+</style>
